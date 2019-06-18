c5481f1789b48421507c11c1ba715fc6069c3f8b
VishalV97 <vv198@scarletmail.rutgers.edu>
2019-06-18 00:21:41 -0400

    Update bisection_CreditPayment.py

--- _code/bisection_CreditPayment.py
+++ _code/bisection_CreditPayment.py
@@ -1,20 +1,26 @@
-balance = 320000
-initial_balance = balance
-annualInterestRate = 0.2
-high = (initial_balance*(1 + annualInterestRate/12.0)**12)/12.0
-lo = initial_balance/12.0
-alpha = 0.01
+def bisec_Credit(balance, annualInterestRate):
+	initial_balance = balance
+	annualInterestRate = 0.2
+	high = (initial_balance*(1 + annualInterestRate/12.0)**12)/12.0
+	lo = initial_balance/12.0
+	alpha = 0.01
 
-while abs(balance) > alpha:
-    monthlyPayment = (lo + high)/2
-    balance = initial_balance
-    for i in range(12):
-        balance = balance - monthlyPayment + ((balance - monthlyPayment)*annualInterestRate/12.0)
-    if balance > alpha:
-        lo = monthlyPayment
-    elif balance < -alpha:
-        high = monthlyPayment
-    else:
-        break
+	while abs(balance) > alpha:
+	    monthlyPayment = (lo + high)/2
+	    balance = initial_balance
+	    for i in range(12):
+	        balance = balance - monthlyPayment + ((balance - monthlyPayment)*annualInterestRate/12.0)
+	    if balance > alpha:
+	        lo = monthlyPayment
+	    elif balance < -alpha:
+	        high = monthlyPayment
+	    else:
+	        break
 
 print("Lowest Payment:", round(monthlyPayment, 2))
+
+'''The function takes in two floats or integers to calculate the lowest amount of money needed to pay off credit
+with a fixed monthly payment amount in one year
+
+Calculated using bisection search.
+'''
